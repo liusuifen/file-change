af78e9487312095306937a56e8ac23e2c148295d
liusuifen <2660444092@qq.com>
2020-04-09 15:21:31 +0800

    提交

---
+++ jvmload/src/main/java/com/example/jvmload/MyClassLoaderTest.java
@@ -0,0 +1,62 @@
+package com.example.jvmload;
+
+import java.io.FileInputStream;
+import java.lang.reflect.Method;
+
+public class MyClassLoaderTest {
+
+    static class MyClassLoader extends ClassLoader {
+         private String classPath;
+         public MyClassLoader(String classPath) {
+             this.classPath = classPath;
+         }
+         private byte[] loadByte(String name) throws Exception {
+             name = name.replaceAll("\\.", "/");
+             FileInputStream fis = new FileInputStream(classPath + "/" + name  + ".class");
+             int len = fis.available();
+             byte[] data = new byte[len];
+             fis.read(data);
+             fis.close();
+             return data;
+         }
+         protected Class<?> findClass(String name) throws ClassNotFoundException {
+             try {
+                 byte[] data = loadByte(name);
+                 //defineClass将一个字节数组转为Class对象，这个字节数组是class文件读取后最终 的字节数组。
+                 return defineClass(name, data, 0, data.length);
+             } catch (Exception e) {
+                 e.printStackTrace();
+                 throw new ClassNotFoundException();
+             }
+         }
+
+        protected Class<?> loadClass(String name, boolean resolve)
+                throws ClassNotFoundException {
+             synchronized (getClassLoadingLock(name)) {
+                 // First, check if the class has already been loaded
+                 Class<?> c = findLoadedClass(name);
+                 if (c == null) {
+                     // If still not found, then invoke findClass in order
+                     // to find the class.
+                     long t1 = System.nanoTime();
+                     c = findClass(name);
+                     // this is the defining class loader; record the stats
+                     sun.misc.PerfCounter.getFindClassTime().addElapsedTimeFrom(t1);
+                     sun.misc.PerfCounter.getFindClasses().increment();
+                 }
+                 if (resolve) {
+                     resolveClass(c);
+                 }
+                 return c;
+             }
+    }
+    public static void main(String args[]) throws Exception {
+        MyClassLoader classLoader = new MyClassLoader("D:/test");
+        Class clazz = classLoader.loadClass("com.tuling.jvm.User1");
+        Object obj = clazz.newInstance();
+        Method method= clazz.getDeclaredMethod("sout", null);
+         method.invoke(obj, null);
+         System.out.println(clazz.getClassLoader().getClass().getName());
+    }
+}
+}
